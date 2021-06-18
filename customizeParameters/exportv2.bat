188c429b442505040f9cd0f7a171afd5c51bc3f6
Olaf Hagendorf <olaf.hagendorf@hs-wismar.de>
2021-06-18 20:45:25 +0200

    reorg examples structure - combine several examples into one repo

--- customizeParameters/exportv2.bat
+++ customizeParameters/exportv2.bat
@@ -4,13 +4,18 @@ rem +++++++++++++++++++++++++++
 
 set mbedos=mbed-os-6.9.0
 
-set platform=NUCLEO_F446RE
+rem set platform=NUCLEO_F446RE
+set platform=NUCLEO_F767ZI
 rem set platform=AUTO
 
 set ide=uvision6
 rem set ide=eclipse_gcc_arm
 rem set ide=sw4stm32
 rem set ide=gcc_arm
+
+set exp=debug
+rem set exp=develop
+rem set exp=release
 
 rem +++++++++++++++++++++++++++
 rem +++++++ config ende +++++++
@@ -26,8 +31,8 @@ echo:"Mbed OS:   "%mbedos%
 echo:"Plattform: "%platform%
 echo:"IDE:       "%ide%
 
-mbed config -G MBED_OS_DIR %projectpath%\%mbedos%
-mbed export -v -m %platform% -i %ide% --source .\%prj% --source .\%mbedos%
+mbed config -G MBED_OS_DIR %projectpath%\..\%mbedos%
+mbed export -v -m %platform% -i %ide% --source .\%prj% --source ..\%mbedos%
 
 cd .\%prj%
 pause 
---
+++ serialCommands/serialCommands.cpp
@@ -0,0 +1,61 @@
+#include "mbed.h"
+#include "CommandParser.h"
+
+typedef CommandParser<> MyCommandParser;
+
+#define BUF_LEN 80
+
+uint8_t idx=0;
+uint8_t buffer0[BUF_LEN];
+uint8_t buffer1[BUF_LEN];
+volatile uint8_t NewData = 0;
+
+UnbufferedSerial rser(USBTX, USBRX);
+MyCommandParser parser;
+
+void serialCb() {
+  char ch;
+  rser.read(&ch, 1);
+  if (ch == 10 || ch == 13) {
+    if (idx > 0) {
+      NewData = 1;
+      memcpy(buffer1, buffer0, idx);
+      buffer1[idx++] = 0;
+    } else {
+      NewData = 0;
+    }
+    idx = 0;
+  } else {
+    if (idx < BUF_LEN) {
+      buffer0[idx++] = ch;
+    }
+    NewData = 0;
+  }
+}
+
+void cmd_test(MyCommandParser::Argument *args, char *response) {
+  printf("string: %s\n", args[0].asString);
+  printf("double: %f\n", args[1].asDouble);
+  printf("int64: %d\n", (int32_t)args[2].asInt64); // NOTE: on older AVR-based boards, Serial doesn't support printing 64-bit values, so we'll cast it down to 32-bit
+  printf("uint64: %d\n", (uint32_t)args[3].asUInt64); // NOTE: on older AVR-based boards, Serial doesn't support printing 64-bit values, so we'll cast it down to 32-bit
+  strlcpy(response, "success", MyCommandParser::MAX_RESPONSE_SIZE);
+}
+
+int main() {
+  char response[MyCommandParser::MAX_RESPONSE_SIZE];
+  
+  rser.baud(115200);
+
+  printf("Start\n");
+
+  parser.registerCommand("TEST", "sdiu", "example: TEST \"def\" -1.234e5 -123 123\n", &cmd_test);
+
+  rser.attach(&serialCb, SerialBase::RxIrq);
+
+  while(true) {
+    while(!NewData) ;
+    parser.processCommand((const char*)buffer1, response);
+    printf("%s\n", response);
+    NewData = 0;
+  }
+}
